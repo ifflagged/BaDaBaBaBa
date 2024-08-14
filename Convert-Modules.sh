742792f56c1d71cd34ab13a0cca77b77560d1616
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-08-14 17:21:11 +0800

    Update Convert-Modules.sh

--- Convert-Modules.sh
+++ Convert-Modules.sh
@@ -3,8 +3,22 @@ input_file=$1
 module_name=$2
 comment=$3
 
+# Common replacements for both Surge and Loon
+sed_common="
+    s/raw.githubusercontent.com/github.com/Ig
+    s/\/main\//\/raw\/main\//Ig
+    s/\/master\//\/raw\/master\//Ig
+    s/HOST,/DOMAIN,/Ig
+    s/HOST-SUFFIX,/DOMAIN-SUFFIX,/Ig
+    s/HOST-KEYWORD,/DOMAIN-KEYWORD,/Ig
+    s/IP-CIDR,/IP-CIDR,/Ig
+    s/IP6-CIDR,/IP-CIDR6,/Ig
+    s/, REJECT/, REJECT, no-resolve/Ig
+"
+
 # Surge conversion
 sed -e "1 i $comment" \
+    -e "$sed_common" \
     -e 's/url reject-dict/- reject/Ig' \
     -e "s/url reject-200/- reject/Ig" \
     -e 's/url reject/- reject/Ig' \
@@ -14,20 +28,12 @@ sed -e "1 i $comment" \
     -e 's/ url script-response-body /,requires-body=1,script-path=/Ig' \
     -e 's/ url script-response-header /,requires-body=0,script-path=/Ig' \
     -e 's/ url script-request-body /,requires-body=1,script-path=/Ig' \
-    -e 's/raw.githubusercontent.com/github.com/Ig' \
-    -e 's/\/main\//\/raw\/main\//Ig' \
-    -e 's/\/master\//\/raw\/master\//Ig' \
-    -e 's/HOST,/DOMAIN,/Ig' \
-    -e 's/HOST-SUFFIX,/DOMAIN-SUFFIX,/Ig' \
-    -e 's/HOST-KEYWORD,/DOMAIN-KEYWORD,/Ig' \
-    -e 's/IP-CIDR,/IP-CIDR,/Ig' \
-    -e 's/IP6-CIDR,/IP-CIDR6,/Ig' \
-    -e 's/, REJECT/, REJECT, no-resolve/Ig' \
     -e 's/hostname =/Hostname = %APPEND%/Ig' \
     $input_file > Modules/Surge/${module_name}.sgmodule
 
 # Loon conversion
 sed -e "1 i $comment" \
+    -e "$sed_common" \
     -e 's/url reject-dict/reject-dict/Ig' \
     -e "s/url reject-200/reject-200/Ig" \
     -e 's/url reject/reject/Ig' \
@@ -39,14 +45,5 @@ sed -e "1 i $comment" \
     -e 's/url script-response-body/script-path=/Ig' \
     -e 's/url script-response-header/script-path=/Ig' \
     -e 's/url script-request-body/script-path=/Ig' \
-    -e 's/raw.githubusercontent.com/github.com/Ig' \
-    -e 's/\/main\//\/raw\/main\//Ig' \
-    -e 's/\/master\//\/raw\/master\//Ig' \
-    -e 's/HOST,/DOMAIN,/Ig' \
-    -e 's/HOST-SUFFIX,/DOMAIN-SUFFIX,/Ig' \
-    -e 's/HOST-KEYWORD,/DOMAIN-KEYWORD,/Ig' \
-    -e 's/IP-CIDR,/IP-CIDR,/Ig' \
-    -e 's/IP6-CIDR,/IP-CIDR6,/Ig' \
-    -e 's/, REJECT/, REJECT, no-resolve/Ig' \
-    -e 's/hostname =/Hostname = %APPEND%/Ig' \
+    -e 's/hostname =/Hostname = /Ig' \
     $input_file > Modules/Loon/${module_name}.plugin

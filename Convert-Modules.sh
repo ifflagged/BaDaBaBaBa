b042eaa8e3a23e15c17db013763607f767a227b1
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-08-16 20:28:58 +0800

    Update Convert-Modules.sh

--- Convert-Modules.sh
+++ Convert-Modules.sh
@@ -20,13 +20,13 @@ sed_common="
 # Surge conversion
 sed -e "1 i $comment" \
     -e "$sed_common" \
-    -e 's/url reject/- reject/Ig' \
     -e "s/url reject-200/- reject/Ig" \
     -e 's/url reject-img/- reject/Ig' \
     -e 's/url reject-dict/- reject/Ig' \
     -e 's/url reject-array/- reject/Ig' \
     -e 's/url reject-video/- reject/Ig' \
     -e 's/url reject-replace/- reject/Ig' \
+    -e 's/url reject/- reject/Ig' \ #放在后面避免影响结果
     -e 's/, REJECT-DROP/, REJECT/Ig' \
     -e "/url script-response-body/ s/^/${module_name} = type=http-response,pattern=/" \
     -e "/url script-response-header/ s/^/${module_name} = type=http-response,pattern=/" \
@@ -40,13 +40,13 @@ sed -e "1 i $comment" \
 # Loon conversion
 sed -e "1 i $comment" \
     -e "$sed_common" \
-    -e 's/url reject/reject/Ig' \
     -e "s/url reject-200/reject-200/Ig" \
     -e 's/url reject-img/reject-img/Ig' \
     -e 's/url reject-dict/reject-dict/Ig' \
     -e 's/url reject-array/reject-array/Ig' \
     -e 's/url reject-video/reject-video/Ig' \
     -e 's/url reject-replace/reject-replace/Ig' \
+    -e 's/url reject/reject/Ig' \ #放在后面避免影响结果
     -e "/url script-response-body/ s/^/http-response /" \
     -e "/url script-response-header/ s/^/http-response /" \
     -e "/url script-request-body/ s/^/http-request /" \

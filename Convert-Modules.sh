b6eee92d2b1e3484467f8fb7dddef1838377826c
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-08-16 20:17:16 +0800

    Update Convert-Modules.sh

--- Convert-Modules.sh
+++ Convert-Modules.sh
@@ -8,20 +8,26 @@ sed_common="
     s/raw.githubusercontent.com/github.com/Ig
     s/\/main\//\/raw\/main\//Ig
     s/\/master\//\/raw\/master\//Ig
+    s/url script-analyze-echo-response/url script-response-body/Ig
     s/HOST,/DOMAIN,/Ig
     s/HOST-SUFFIX,/DOMAIN-SUFFIX,/Ig
     s/HOST-KEYWORD,/DOMAIN-KEYWORD,/Ig
     s/IP-CIDR,/IP-CIDR,/Ig
     s/IP6-CIDR,/IP-CIDR6,/Ig
-    s/, REJECT/, REJECT, no-resolve/Ig
+    /IP-CIDR/ s/, REJECT/, REJECT, no-resolve/Ig
 "
 
 # Surge conversion
 sed -e "1 i $comment" \
     -e "$sed_common" \
-    -e 's/url reject-dict/- reject/Ig' \
-    -e "s/url reject-200/- reject/Ig" \
     -e 's/url reject/- reject/Ig' \
+    -e "s/url reject-200/- reject/Ig" \
+    -e 's/url reject-img/- reject/Ig' \
+    -e 's/url reject-dict/- reject/Ig' \
+    -e 's/url reject-array/- reject/Ig' \
+    -e 's/url reject-video/- reject/Ig' \
+    -e 's/url reject-replace/- reject/Ig' \
+    -e 's/, REJECT-DROP/, REJECT/Ig' \
     -e "/url script-response-body/ s/^/${module_name} = type=http-response,pattern=/" \
     -e "/url script-response-header/ s/^/${module_name} = type=http-response,pattern=/" \
     -e "/url script-request-body/ s/^/${module_name} = type=http-request,pattern=/" \
@@ -34,9 +40,13 @@ sed -e "1 i $comment" \
 # Loon conversion
 sed -e "1 i $comment" \
     -e "$sed_common" \
-    -e 's/url reject-dict/reject-dict/Ig' \
-    -e "s/url reject-200/reject-200/Ig" \
-    -e 's/url reject/reject/Ig' \
+    -e 's/url reject/- reject/Ig' \
+    -e "s/url reject-200/- reject-200/Ig" \
+    -e 's/url reject-img/- reject-img/Ig' \
+    -e 's/url reject-dict/- reject-dict/Ig' \
+    -e 's/url reject-array/- reject-array/Ig' \
+    -e 's/url reject-video/- reject-video/Ig' \
+    -e 's/url reject-replace/- reject-replace/Ig' \
     -e "/url script-response-body/ s/^/http-response /" \
     -e "/url script-response-header/ s/^/http-response /" \
     -e "/url script-request-body/ s/^/http-request /" \

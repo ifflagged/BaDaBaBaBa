1ff6724edaa66296e0241b5c7a6f1313cb05afb0
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-08-27 18:01:14 +0800

    Create Convert-Modules-827.sh

---
+++ Convert-Modules-827.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+input_file=$1
+module_name=$2
+comment=$3
+
+# Common replacements for both Surge and Loon
+sed_common="
+    /raw.githubusercontent.com/ s/\/main\//\/raw\/main\//Ig
+    /raw.githubusercontent.com/ s/\/master\//\/raw\/master\//Ig
+    s/raw.githubusercontent.com/github.com/Ig
+    s/url script-analyze-echo-response/url script-request-body/Ig
+    s/HOST,/DOMAIN,/Ig
+    s/HOST-SUFFIX,/DOMAIN-SUFFIX,/Ig
+    s/HOST-KEYWORD,/DOMAIN-KEYWORD,/Ig
+    s/IP-CIDR,/IP-CIDR,/Ig
+    s/IP6-CIDR,/IP-CIDR6,/Ig
+    /IP-CIDR/ s/, REJECT/, REJECT, no-resolve/Ig
+"
+
+# Surge conversion
+sed -e "1 i\\
+$comment" \
+    -e "$sed_common" \
+    -e "s/url reject-200/- reject/Ig" \
+    -e 's/url reject-img/- reject/Ig' \
+    -e 's/url reject-dict/- reject/Ig' \
+    -e 's/url reject-array/- reject/Ig' \
+    -e 's/url reject-video/- reject/Ig' \
+    -e 's/url reject-replace/- reject/Ig' \
+    -e 's/url reject/- reject/Ig' \
+    -e 's/, REJECT-DROP/, REJECT/Ig' \
+    -e "/url script-response-body/ s/^/${module_name} = type=http-response,pattern=/" \
+    -e "/url sscript-echo-response/ s/^/${module_name} = type=http-response,pattern=/" \
+    -e "/url script-response-header/ s/^/${module_name} = type=http-response,pattern=/" \
+    -e "/url script-request-body/ s/^/${module_name} = type=http-request,pattern=/" \
+    -e "/url script-request-header/ s/^/${module_name} = type=http-request,pattern=/" \
+    -e "/url script-analyze-echo-response/ s/^/${module_name} = type=http-request,pattern=/" \
+    -e 's/ url script-response-body /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-echo-response /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-response-header /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-request-body /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-analyze-echo-response /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-request-header /,requires-body=0,script-path=/Ig' \
+    -e 's/hostname =/Hostname = %APPEND%/Ig' \
+    "$input_file" > "Modules/Surge/${module_name}.conf"
+
+# Loon conversion
+sed -e "1 i\\
+$comment" \
+    -e "$sed_common" \
+    -e "s/url reject-200/- reject/Ig" \
+    -e 's/url reject-img/- reject/Ig' \
+    -e 's/url reject-dict/- reject/Ig' \
+    -e 's/url reject-array/- reject/Ig' \
+    -e 's/url reject-video/- reject/Ig' \
+    -e 's/url reject-replace/- reject/Ig' \
+    -e 's/url reject/- reject/Ig' \
+    -e 's/, REJECT-DROP/, REJECT/Ig' \
+    -e "/url script-response-body/ s/^/${module_name} = type=http-response,pattern=/" \
+    -e "/url sscript-echo-response/ s/^/${module_name} = type=http-response,pattern=/" \
+    -e "/url script-response-header/ s/^/${module_name} = type=http-response,pattern=/" \
+    -e "/url script-request-body/ s/^/${module_name} = type=http-request,pattern=/" \
+    -e "/url script-request-header/ s/^/${module_name} = type=http-request,pattern=/" \
+    -e "/url script-analyze-echo-response/ s/^/${module_name} = type=http-request,pattern=/" \
+    -e 's/ url script-response-body /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-echo-response /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-response-header /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-request-body /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-analyze-echo-response /,requires-body=1,script-path=/Ig' \
+    -e 's/ url script-request-header /,requires-body=0,script-path=/Ig' \
+    -e 's/hostname =/Hostname = %APPEND%/Ig' \
+    "$input_file" > "Modules/Loon/${module_name}.conf"

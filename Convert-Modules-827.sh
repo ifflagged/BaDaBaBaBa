d1c88f36014a58e06b9d5b8d1f0e7b04ffd71050
ifflagged <91680218+ifflagged@users.noreply.github.com>
2024-08-27 18:19:48 +0800

    Update Convert-Modules-827.sh

--- Convert-Modules-827.sh
+++ Convert-Modules-827.sh
@@ -48,25 +48,30 @@ $comment" \
 sed -e "1 i\\
 $comment" \
     -e "$sed_common" \
-    -e "s/url reject-200/- reject/Ig" \
-    -e 's/url reject-img/- reject/Ig' \
-    -e 's/url reject-dict/- reject/Ig' \
-    -e 's/url reject-array/- reject/Ig' \
-    -e 's/url reject-video/- reject/Ig' \
-    -e 's/url reject-replace/- reject/Ig' \
-    -e 's/url reject/- reject/Ig' \
-    -e 's/, REJECT-DROP/, REJECT/Ig' \
-    -e "/url script-response-body/ s/^/${module_name} = type=http-response,pattern=/" \
-    -e "/url sscript-echo-response/ s/^/${module_name} = type=http-response,pattern=/" \
-    -e "/url script-response-header/ s/^/${module_name} = type=http-response,pattern=/" \
-    -e "/url script-request-body/ s/^/${module_name} = type=http-request,pattern=/" \
-    -e "/url script-request-header/ s/^/${module_name} = type=http-request,pattern=/" \
-    -e "/url script-analyze-echo-response/ s/^/${module_name} = type=http-request,pattern=/" \
-    -e 's/ url script-response-body /,requires-body=1,script-path=/Ig' \
-    -e 's/ url script-echo-response /,requires-body=1,script-path=/Ig' \
-    -e 's/ url script-response-header /,requires-body=1,script-path=/Ig' \
-    -e 's/ url script-request-body /,requires-body=1,script-path=/Ig' \
-    -e 's/ url script-analyze-echo-response /,requires-body=1,script-path=/Ig' \
-    -e 's/ url script-request-header /,requires-body=0,script-path=/Ig' \
-    -e 's/hostname =/Hostname = %APPEND%/Ig' \
+    -e "s/url reject-200/reject-200/Ig" \
+    -e 's/url reject-img/reject-img/Ig' \
+    -e 's/url reject-dict/reject-dict/Ig' \
+    -e 's/url reject-array/reject-array/Ig' \
+    -e 's/url reject-video/reject-video/Ig' \
+    -e 's/url reject-replace/reject-replace/Ig' \
+    -e 's/url reject/reject/Ig' \
+    -e "/url script-response-body/ s/^/http-response /" \
+    -e "/url sscript-echo-response/ s/^/http-response /" \
+    -e "/url script-response-header/ s/^/http-response /" \
+    -e "/url script-request-body/ s/^/http-request /" \
+    -e "/url script-request-header/ s/^/http-request /" \
+    -e "/url script-analyze-echo-response/ s/^/http-request /" \
+    -e "/script-response-body/ s/$/, requires-body = true, tag = ${module_name}/" \
+    -e "/script-echo-response/ s/$/, requires-body = true, tag = ${module_name}/" \
+    -e "/script-response-header/ s/$/, requires-body = true, tag = ${module_name}/" \
+    -e "/script-request-body/ s/$/, requires-body = true, tag = ${module_name}/" \
+    -e "/script-analyze-echo-response/ s/$/, requires-body = true, tag = ${module_name}/" \
+    -e "/script-request-header/ s/$/, requires-body = false, tag = ${module_name}/" \
+    -e 's/url script-response-body/script-path=/Ig' \
+    -e 's/url script-echo-response/script-path=/Ig' \
+    -e 's/url script-response-header/script-path=/Ig' \
+    -e 's/url script-request-body/script-path=/Ig' \
+    -e 's/url script-request-header/script-path=/Ig' \
+    -e 's/url script-analyze-echo-response/script-path=/Ig' \
+    -e 's/hostname =/Hostname =/Ig' \
     "$input_file" > "Modules/Loon/${module_name}.plugin"
